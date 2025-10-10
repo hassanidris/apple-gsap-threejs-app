948bdf65c788e2f2f5afa51970ea6057d34ada5e
Hassen Ahmed <hassan.creative@gmail.com>
2025-10-10 21:30:03 +0200

    setuping project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-indigo-700">
+      Hello, Apple Mackbook Pro!
+    </h1>
+  );
 };
 
 export default App;
